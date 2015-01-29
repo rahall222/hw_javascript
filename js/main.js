457e26766378a778169d702731952b11b2f7918d
Rebecca Hall <rebecca@doublestruck.co.uk>
2015-01-29 14:40:31 +0000

    create objects for 5 prsidents, create array of objects

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var president1 = {firstName:"Barack", lastName:"Obama", termLength:2 , party:"Democratic", yearsOfPresidency:7};
+var president2 = {firstName:"George", lastName:"Bush", termLength:2 , party:"Republican", yearsOfPresidency:8};
+var president3 = {firstName:"Bill", lastName:"Clinton", termLength:2 , party:"Democratic", yearsOfPresidency:8};
+var president4 = {firstName:"George", lastName:"Bush", termLength:1 , party:"Republican", yearsOfPresidency:4};
+var president5 = {firstName:"Ronald", lastName:"Reagan", termLength:2 , party:"Republican", yearsOfPresidency:8};
+
+var presidents = [president1, president2, president3, president4, president5]
+
+console.log()
