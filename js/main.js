251f6c65ba039416eafd59ad6f074cbc8fd2d1f4
Rebecca Hall <rebecca@doublestruck.co.uk>
2015-01-29 14:59:49 +0000

    trying to write console.log() but so lost

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@ var president3 = {firstName:"Bill", lastName:"Clinton", termLength:2 , party:"De
 var president4 = {firstName:"George", lastName:"Bush", termLength:1 , party:"Republican", yearsOfPresidency:4};
 var president5 = {firstName:"Ronald", lastName:"Reagan", termLength:2 , party:"Republican", yearsOfPresidency:8};
 
-var presidents = [president1, president2, president3, president4, president5]
+var presidents = [president1, president2, president3, president4, president5];
 
-console.log()
+console.log(presidents) {return president3.firstName + " " + president3.lastName;}
