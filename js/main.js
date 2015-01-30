16ad3d6d6179c134bb483c84912330a5e1925457
Rebecca Hall <rebecca@doublestruck.co.uk>
2015-01-30 10:00:15 +0000

    minimize code, fix console.log function and test

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-var president1 = {firstName:"Barack", lastName:"Obama", termLength:2 , party:"Democratic", yearsOfPresidency:7};
-var president2 = {firstName:"George", lastName:"Bush", termLength:2 , party:"Republican", yearsOfPresidency:8};
-var president3 = {firstName:"Bill", lastName:"Clinton", termLength:2 , party:"Democratic", yearsOfPresidency:8};
-var president4 = {firstName:"George", lastName:"Bush", termLength:1 , party:"Republican", yearsOfPresidency:4};
-var president5 = {firstName:"Ronald", lastName:"Reagan", termLength:2 , party:"Republican", yearsOfPresidency:8};
+var presidents = [
+	{firstName:"Barack", lastName:"Obama", termLength:2 , party:"Democratic", yearsOfPresidency:7},
+	{firstName:"George", lastName:"Bush", termLength:2 , party:"Republican", yearsOfPresidency:8},
+	{firstName:"Bill", lastName:"Clinton", termLength:2 , party:"Democratic", yearsOfPresidency:8},
+	{firstName:"George", lastName:"Bush", termLength:1 , party:"Republican", yearsOfPresidency:4},
+	{firstName:"Ronald", lastName:"Reagan", termLength:2 , party:"Republican", yearsOfPresidency:8}
+]
 
-var presidents = [president1, president2, president3, president4, president5];
-
-console.log(presidents) {return president3.firstName + " " + president3.lastName;}
+console.log(presidents[2].firstName + " " + presidents[2].lastName);
