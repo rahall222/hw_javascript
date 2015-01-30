747d3afcb12cf16873ee2d88cf8b3d032e972708
Rebecca Hall <rebecca@doublestruck.co.uk>
2015-01-30 12:12:59 +0000

    gave up part 2 move to part three inception

--- js/main.js
+++ js/main.js
@@ -3,3 +3,26 @@ function howdy() {
 }
 
 howdy();
+
+//var numba = function(string) {
+//	if (string < 7) {
+//		console.log("What a short little word!");
+//	} else if (string > 7) {
+//		console.log("I’m sorry, but that’s too many to count.");
+//	} else {
+//		console.log("7, what a perfect choice!");
+//	}
+//}
+
+//numba("university");
+
+function inception(callback, value) {
+	callback(value);
+}
+
+function theCallback(value) {
+	console.log(value + " is a fantastic movie");
+}
+
+inception( theCallback, "Indiana Jones and the Temple of Doom");
+
