13b4ff24eca4ea8380b8324cbad74b01d5b88391
Rebecca Hall <rebecca@doublestruck.co.uk>
2015-01-30 17:05:15 +0000

    fixed second function added string.length

--- js/main.js
+++ js/main.js
@@ -4,17 +4,19 @@ function howdy() {
 
 howdy();
 
-//var numba = function(string) {
-//	if (string < 7) {
-//		console.log("What a short little word!");
-//	} else if (string > 7) {
-//		console.log("I’m sorry, but that’s too many to count.");
-//	} else {
-//		console.log("7, what a perfect choice!");
-//	}
-//}
+var numba = function(string) {
+	if (string.length < 7) {
+		console.log("What a short little word!");
+	} else if (string.length > 7) {
+		console.log("I’m sorry, but that’s too many to count.");
+	} else {
+		console.log("7, what a perfect choice!");
+	}
+}
 
-//numba("university");
+numba("7");
+numba("seventy");
+numba("university");
 
 function inception(callback, value) {
 	callback(value);
